feb6dcf167c3b6ec506806361439fd79a9a40abd
nengyi1226 <124417045+nengyi1226@users.noreply.github.com>
2023-07-01 19:06:59 -0700

    Update msf-countdown.sh

--- msf-countdown.sh
+++ msf-countdown.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-echo "选择目标平台架构: "
-echo "1) x86"
-echo "2) x64"
-while true; do
-  read -p ": " option
-  case $option in
-    1 ) platform="x86"; break;;
-    2 ) platform="x64"; break;;
-    * ) echo "无效输入，请重新选择。";;
-  esac
-done
+read -p "目标平台架构: " platform
 
 echo "载体名，无需输入后缀，本程序将自动补齐"
 read -p "载体名: " template
